b5a192e95b0c20b46584926d9dea4072cb899995
jove <87570166+27tape@users.noreply.github.com>
2021-09-05 14:39:19 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,22 +1,22 @@
 const body = document.querySelector("body");
 
-// 이미지 갯수
+/* 이미지 갯수 */
 const IMG_NUMBER=5;
 
-// 랜덤 숫자 생성
+/* 랜덤 숫자 생성 */
 function genRandom(){
     return Math.ceil(Math.random()* IMG_NUMBER);
 }
 
-// 랜덤 이미지 출력
+/* 랜덤 이미지 출력 */
 function paintImage(imgNumber) {
     const img = new Image();
-    img.src = `img/${imgNumber}.jpg`;
+    img.src = `assets/${imgNumber}.jpg`;
     img.classList.add("bgImg");
     body.prepend(img);
 }
 
-// 랜덤 수 생성해서, 랜덤 이미지 출력하기
+/* 랜덤 수 생성해서, 랜덤 이미지 출력하기 */
 function init() {
     const randomNumber = genRandom();
     paintImage(randomNumber);
